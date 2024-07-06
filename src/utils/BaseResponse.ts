b197bdda9f581097ddc2a5d0946ba285c8a5279b
vetonmilaimi <vetonmila@gmail.com>
2024-07-06 23:15:51 +0200

    BaseResponse Class and error handler added

---
+++ src/utils/BaseResponse.ts
@@ -0,0 +1,42 @@
+import { Response } from 'express'
+import { BaseError, InternalServerError, JoiError, RouteNotFoundError } from './exceptions'
+
+import { ExpressJoiError } from 'express-joi-validation'
+
+class BaseResponse {
+  private response: Response
+  constructor(response: Response) {
+    this.response = response
+  }
+
+  private handleError(err: unknown) {
+    console.log(err)
+    let errOccurred
+    if (err instanceof BaseError) {
+      errOccurred = err
+    } else if ((err as ExpressJoiError)?.error?.isJoi) {
+      errOccurred = new JoiError(err as ExpressJoiError)
+    } else {
+      errOccurred = new InternalServerError()
+    }
+    return this.response.status(errOccurred?.statusCode).json(errOccurred)
+  }
+
+  public success(body: object | string | number | boolean | null) {
+    this.response.status(200).json({
+      error: false,
+      message: body,
+    })
+  }
+
+  public error(err: unknown) {
+    this.handleError(err)
+  }
+
+  public routeNotFound() {
+    const err = new RouteNotFoundError(this?.response?.req?.originalUrl || '')
+    this.handleError(err)
+  }
+}
+
+export default (response: Response) => new BaseResponse(response)
---
+++ src/utils/exceptions.ts
@@ -0,0 +1,45 @@
+import { ExpressJoiError } from 'express-joi-validation'
+import { ValidationError } from 'joi'
+
+export class BaseError extends Error {
+  public error: boolean = true
+  public name: string = 'employees-management-api-error'
+  public message: string = 'Server Error'
+  public statusCode: number = 400
+  public details: unknown
+}
+
+export class InternalServerError extends BaseError {
+  name = 'internal-server-error'
+  statusCode = 400
+
+  constructor() {
+    super()
+  }
+}
+
+export class JoiError extends BaseError {
+  name = 'validation-error'
+  message = 'Validation error.'
+  statusCode = 400
+
+  constructor(joiError: ExpressJoiError | ValidationError) {
+    super()
+    const details = (joiError as ExpressJoiError)?.error?.details
+      ? (joiError as ExpressJoiError)?.error?.details
+      : (joiError as ValidationError)?.details
+    this.details = { validation_errors: details }
+  }
+}
+
+export class RouteNotFoundError extends BaseError {
+  name = 'route-not-found-error'
+  message = 'This route could not be found!'
+  statusCode = 404
+  path: string
+
+  constructor(path: string) {
+    super()
+    this.path = path
+  }
+}
